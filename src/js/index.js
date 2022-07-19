6a1dc7b4d0dbae2559618136ff05721944d06b41
irinkwink <irina-ns@yandex.ru>
2022-07-19 16:20:35 +0300

    feat: add faq and footer (html, style, js)

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,11 @@
+window.onload = function(e) {
+  const message = document.createElement('p');
+  message.className = 'message';
+  message.textContent = 'Данный сайт создан только в учебных целях. На нем не производится покупка или обмен криптовалюты';
+
+  document.body.insertBefore(message, document.body.header);
+}  
+
 const headerNavBtn = document.querySelector('.header__nav-btn');
 const headerNavigation = document.querySelector('.header__navigation');
 
@@ -32,4 +40,23 @@ options.addEventListener('click', (e) => {
 
   }
 });
+
+const faqList = document.querySelector('.faq__list');
+
+faqList.addEventListener('click', (e) => {
+  if (!e.target.classList.contains('faq__list')) {
+
+    const item = e.target.closest('.faq__item')
+
+    const title = item.querySelector('.faq__item-title');
+    title.classList.toggle('faq__item-title_active');
+
+    const description = item.querySelector('.faq__item-description');
+    description.classList.toggle('faq__item-description_active');
+
+    const btn = item.querySelector('.faq__btn');
+    btn.classList.toggle('faq__btn_active');
+  
+  }
+})
 
