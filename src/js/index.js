3f199516da35ccb3416632d11a1ae019f37755d4
irinkwink <irina-ns@yandex.ru>
2022-07-19 16:50:10 +0300

    fix: faq js

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,10 @@
-window.onload = function(e) {
-  const message = document.createElement('p');
-  message.className = 'message';
-  message.textContent = 'Данный сайт создан только в учебных целях. На нем не производится покупка или обмен криптовалюты';
+// window.onload = function(e) {
+//   const message = document.createElement('p');
+//   message.className = 'message';
+//   message.textContent = 'Данный сайт создан только в учебных целях. На нем не производится покупка или обмен криптовалюты';
 
-  document.body.insertBefore(message, document.body.header);
-}  
+//   document.body.insertBefore(message, document.body.header);
+// }  
 
 const headerNavBtn = document.querySelector('.header__nav-btn');
 const headerNavigation = document.querySelector('.header__navigation');
@@ -44,10 +44,10 @@ options.addEventListener('click', (e) => {
 const faqList = document.querySelector('.faq__list');
 
 faqList.addEventListener('click', (e) => {
-  if (!e.target.classList.contains('faq__list')) {
+  const item = e.target.closest('.faq__item');
 
-    const item = e.target.closest('.faq__item')
-
+  if (item) {
+    console.log('vze');
     const title = item.querySelector('.faq__item-title');
     title.classList.toggle('faq__item-title_active');
 
